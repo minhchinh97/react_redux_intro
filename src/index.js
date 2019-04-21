aab83d249dc421d4f38ac63ba3a1a3436d88ddee
minhchinh97h <minhchinhduong97@gmail.com>
2019-04-21 16:52:24 +0300

    Update PropTypes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
+import rootReducer from './reducers'
 
-
-const store = createStore()
+const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
