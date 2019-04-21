d7b576073e95d50414fb97c1f798ed6baa735dd0
minhchinh97h <minhchinhduong97@gmail.com>
2019-04-21 16:16:59 +0300

    Update reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+
+const store = createStore()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
